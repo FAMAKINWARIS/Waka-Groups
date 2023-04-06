920aa763d28efdecd15144e375bb92950641de53
dadasamuel <124896902+dadasamuel@users.noreply.github.com>
2023-04-06 06:46:26 +0100

    feat "update inventory"

--- model/contollers/inventory.controller.js
+++ model/contollers/inventory.controller.js
@@ -19,5 +19,30 @@ const createInventory = async () => {
   }
 };
 
+
+exports.updateInventory = async (req, res) => {
+  const productId = req.params.id;
+  const { item_name, item_description, item_category, item_amount } = req.body;
+
+  try {
+    const query = 'UPDATE inventory SET item_name = $1, item_description = $2, item_category = $3, item_amount = $4 WHERE id = $5';
+
+    // Execute the query with the parameters
+    await pool.query(query, [item_name, item_description, item_category, item_amount, productId]);
+
+    // Release the inventory back to the pool
+    pool.release();
+
+    res.status(200).json({
+     message: 'Product updated successfully', updateInventory
+    });
+  } catch (err) {
+    res.status(500).json({
+      message:'Error updating product',
+      error: error.message
+    });
+  }
+};
+
 createInventory();
 
